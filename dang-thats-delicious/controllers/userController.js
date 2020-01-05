0380091a63e3a31443e0730cc5e8ba0f39226349
jcgoodwin <jcgoodwin86@users.noreply.github.com>
2020-01-05 16:06:24 -0500

    update

---
+++ dang-thats-delicious/controllers/userController.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+exports.loginForm = (req, res) => {
+  res.render('login', { title: 'login' });
+};
+
+exports.registerForm = (req, res) => {
+  res.render('register', { title: 'Register' });
+};
+
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody('name');
+  req.checkBody('name', 'You must supply a name!').notEmpty();
+  req.checkBody('email', 'That email is not valid!!').isEmail();
+  req.sanitizeBody('email').normalizeEmail({
+    remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false,
+  });
+  req.checkBody('password', 'Password cannot be Blank!').notEmpty();
+  req
+    .checkBody('password-confirm', 'Confirmed Password cannot be Blank!')
+    .notEmpty();
+  req
+    .checkBody('password-confirm', 'Opps! Your passwords do not match')
+    .equals(req.body.password);
+
+  const errors = req.validationErrors();
+  if (errors) {
+    req.flash(
+      'error',
+      errors.map(err => err.msg)
+    );
+    res.render('register', {
+      title: 'Register',
+      body: req.body,
+      flashes: req.flash(),
+    });
+    return; // Stop the fn from running
+  }
+  next();
+};
